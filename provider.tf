a6aa870223f9017a55563efb1f8f81aced65c83c
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-15 11:33:14 -0400

    add vault evrything

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
-#provider "vault" {
- # address         = "https://vault-internal.devopssz14.online:8200"
-  #skip_tls_verify = true
+provider "vault" {
+  address         = "https://vault-internal.devopssz14.online:8200"
+  skip_tls_verify = true
 
-  #token           = var.vault_token
-#}
+  token           = var.vault_token
+}
 

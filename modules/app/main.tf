025852d19725f541eb6ca13ff31aff8195c2bc42
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-16 10:01:35 -0400

    edit security group

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami                     = data.aws_ami.ami.image_id
   instance_type           = var.instance_type
-  vpc_security_group_ids  = [aws_security_group.main.id]
+  #vpc_security_group_ids  = [aws_security_group.main.id]
 
  tags = {
    Name    = var.component

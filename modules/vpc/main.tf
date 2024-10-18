0626a8418d03fa86a87727b0ce7086b8988df3a0
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-18 16:29:44 -0400

    add frontend,backend, db, public subnet route table association to all in main.tf vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,6 +55,14 @@ resource "aws_route_table" "frontend" {
     Name = "${var.env}-frontend-rt-${count.index}"
   }
 }
+
+resource "aws_route_table_association" "frontend" {
+  count = length(var.frontend_subnets)
+  subnet_id = aws_subnet.frontend[count.index].id
+  route_table_id = aws_route_table.frontend[count.index].id
+}
+
+
 resource "aws_subnet" "backend" {
   count              = length(var.backend_subnets)
   vpc_id             = aws_vpc.main.id
@@ -79,6 +87,12 @@ resource "aws_route_table" "backend" {
   }
 }
 
+resource "aws_route_table_association" "backend" {
+  count = length(var.backend_subnets)
+  subnet_id = aws_subnet.backend[count.index].id
+  route_table_id = aws_route_table.backend[count.index].id
+}
+
 resource "aws_subnet" "db" {
   count             = length(var.db_subnets)
   vpc_id            = aws_vpc.main.id
@@ -101,6 +115,14 @@ resource "aws_route_table" "db" {
     Name = "${var.env}-db-rt-${count.index}"
   }
 }
+
+
+resource "aws_route_table_association" "db" {
+  count = length(var.db_subnets)
+  subnet_id = aws_subnet.db[count.index].id
+  route_table_id = aws_route_table.db[count.index].id
+}
+
 resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
   vpc_id            = aws_vpc.main.id
@@ -123,6 +145,12 @@ resource "aws_route_table" "public" {
   tags = {
     Name = "${var.env}-public-rt-${count.index}"
   }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
 }
 
 resource "aws_route" "default-vpc" {

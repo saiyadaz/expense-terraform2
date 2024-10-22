e66a7ce20e562da62bb934bbd1be2cee283dd33c
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-22 11:28:21 -0400

    adding count so that the lb is needed only for frontend and backend subnets

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -75,6 +75,7 @@ resource "aws_route53_record" "record" {
 }
 
 resource "aws_lb" "main" {
+  count               = var.lb_needed
   name                = "${var.component}-${var.env}-alb"
   internal            = var.lb_type =="public"? false : true
   load_balancer_type  = "application"

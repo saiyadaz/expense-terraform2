4c292a62b0039764d4a78fa85a367ad48a6f40e1
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-22 22:27:01 -0400

    added bastion nodes --the port shud open only to workstation so /32 with private ip of bastion nodes --in security groups

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 module "frontend" {
   depends_on   = [module.backend]
 
-instance_type  = var.instance_type
- source        = "./modules/app"
- component     = "frontend"
-  env          = var.env
-  zone_id      = var.zone_id
-  ssh_user     = var.ssh_user
-  ssh_pass     = var.ssh_pass
-  vault_token  = var.vault_token
-  subnets      = module.vpc.frontend_subnets
-  vpc_id       = module.vpc.vpc_id
-  lb_type      = "public"
-  lb_needed    = true
-  lb_subnets   = module.vpc.public_subnets
-  app_port     = 80
+instance_type   = var.instance_type
+ source         = "./modules/app"
+ component      = "frontend"
+  env           = var.env
+  zone_id       = var.zone_id
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
+  vault_token   = var.vault_token
+  subnets       = module.vpc.frontend_subnets
+  vpc_id        = module.vpc.vpc_id
+  lb_type       = "public"
+  lb_needed     = true
+  lb_subnets    = module.vpc.public_subnets
+  app_port      = 80
   bastion_nodes =  var.bastion_nodes
 
 }
 
 module "backend" {
-  depends_on   = [module.mysql]
+  depends_on  = [module.mysql]
 
 source        = "./modules/app"
 instance_type = var.instance_type
@@ -32,10 +32,10 @@ zone_id       = var.zone_id
 vault_token   = var.vault_token
 subnets       = module.vpc.db_subnets
 vpc_id        = module.vpc.vpc_id
-lb_type      = "private"
-lb_needed  = true
-lb_subnets = module.vpc.backend_subnets
-app_port = 8080
+lb_type       = "private"
+lb_needed     = true
+lb_subnets    = module.vpc.backend_subnets
+app_port      = 8080
 bastion_nodes =  var.bastion_nodes
 }
 
@@ -51,7 +51,7 @@ module "mysql" {
   vault_token       = var.vault_token
   subnets           = module.vpc.db_subnets
   vpc_id            = module.vpc.vpc_id
-  bastion_nodes =  var.bastion_nodes
+  bastion_nodes     =  var.bastion_nodes
   }
 
 module "vpc" {

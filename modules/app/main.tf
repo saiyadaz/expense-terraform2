bf282a96f1cce905e6a79627e79718187fff00b7
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-23 10:02:52 -0400

    change name of load balancer

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -101,8 +101,8 @@ resource "aws_route53_record" "load-balancer" {
 }
 resource "aws_security_group" "load-balancer" {
  count           = var.lb_needed ? 1 : 0
-  name           = "${var.component}-${var.env}-sg"
-  description    = "${var.component}-${var.env}-sg"
+  name           = "${var.component}-${var.env}-lb-sg"
+  description    = "${var.component}-${var.env}-lb-sg"
   vpc_id         =  var.vpc_id
 
 

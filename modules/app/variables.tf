914e104994bce31378b68f35a2c4ab9e90b15e81
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-24 12:34:09 -0400

    gic default to lb ports

--- modules/app/variables.tf
+++ modules/app/variables.tf
@@ -22,10 +22,12 @@ variable "app_port" {
 variable "bastion_nodes" {}
 variable "prometheus_nodes" {}
 variable "lb_app_port_sg_cidr" {
-  default = null
+  default = {}
 }
 variable "server_app_port_sg_cidr" {}
 variable "certificate_arn" {
   default = null
 }
-variable "lb_ports" {}
+variable "lb_ports" {
+  default = {}
+}

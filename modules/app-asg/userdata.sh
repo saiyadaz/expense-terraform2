0ee3d0d705311e8684a672e0c7164ee78e3df2a9
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-30 10:47:03 -0400

    edit userdata.sh

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/saiyadaz/expense-ansible2.git get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/saiyadaz/expense-ansible2.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log

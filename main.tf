e9fa446ea6acf761526ba19a863b85948307a016
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-30 11:49:17 -0400

    add main.tf-asg-lb

--- main.tf
+++ main.tf
@@ -52,35 +52,47 @@
 #}
 ##
 module "frontend" {
-  source                  = "./modules/app-asg"
-  app_port                = 80
-  bastion_nodes           = var.bastion_nodes
-  component               = "frontend"
-  env                     = var.env
-  instance_type           = var.instance_type
-  max_capacity            = var.max_capacity
-  min_capacity            = var.min_capacity
-  prometheus_nodes        = var.prometheus_nodes
-  server_app_port_sg_cidr = var.public_subnets
-  subnets                 = module.vpc.frontend_subnets
-  vpc_id                  = module.vpc.vpc_id
-  vault_token             = var.vault_token
+  source                    = [module.backend]
+  app_port                  = 80
+  bastion_nodes             = var.bastion_nodes
+  component                 = "frontend"
+  env                       = var.env
+  instance_type             = var.instance_type
+  max_capacity              = var.max_capacity
+  min_capacity              = var.min_capacity
+  prometheus_nodes          = var.prometheus_nodes
+  server_app_port_sg_cidr   = var.public_subnets
+  subnets                   = module.vpc.frontend_subnets
+  vpc_id                    = module.vpc.vpc_id
+  vault_token               = var.vault_token
+  certificate_arn           = var.certificate_arn
+  lb_app_port_sg_cidr       = ["0.0.0.0/0"]
+  lb_ports                  = { http : 80, https : 443 }
+  lb_subnets                = module.vpc.public_subnets
+  lb_type                   = "public"
+  zone_id                   = var.zone_id
 
 }
 module "backend" {
-  source                  = "./modules/app-asg"
-  app_port                = 8080
-  bastion_nodes           = var.bastion_nodes
-  component               = "backend"
-  env                     = var.env
-  instance_type           = var.instance_type
-  max_capacity            = var.max_capacity
-  min_capacity            = var.min_capacity
-  prometheus_nodes        = var.prometheus_nodes
-  server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
-  subnets                 = module.vpc.backend_subnets
-  vpc_id                  = module.vpc.vpc_id
-  vault_token             = var.vault_token
+  source                    = "./modules/app-asg"
+  app_port                  = 8080
+  bastion_nodes             = var.bastion_nodes
+  component                 = "backend"
+  env                       = var.env
+  instance_type             = var.instance_type
+  max_capacity              = var.max_capacity
+  min_capacity              = var.min_capacity
+  prometheus_nodes          = var.prometheus_nodes
+  server_app_port_sg_cidr   = concat(var.frontend_subnets, var.backend_subnets)
+  subnets                   = module.vpc.backend_subnets
+  vpc_id                    = module.vpc.vpc_id
+  vault_token               = var.vault_token
+  certificate_arn           = var.certificate_arn
+  lb_app_port_sg_cidr       = var.frontend_subnets
+  lb_ports                  = { http : 8080 }
+  lb_subnets                = module.vpc.backend_subnets
+  lb_type                   = "private"
+  zone_id                   = var.zone_id
 
 }
 #concat done because lb --backend subnets needs to access backend subnets and frontend subnets
@@ -100,6 +112,12 @@ module "rds" {
   subnet_ids              = module.vpc.db_subnets
   vpc_id                  = module.vpc.vpc_id
   kms_key_id              = var.kms_key_id
+  certificate_arn         = var.certificate_arn
+  lb_app_port_sg_cidr     = var.frontend_subnets
+  lb_ports                = { http : 8080 }
+  lb_subnets              = module.vpc.backend_subnets
+  lb_type                 = "private"
+  zone_id                 = var.zone_id
 }
 
 #module "mysql" {

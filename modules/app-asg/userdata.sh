592d33fd9c414084f16d8c2882339d8e8bdb9c65
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-01 10:41:12 -0400

    no need ansible for own ami so remove from userdata

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/saiyadaz/expense-ansible2.git get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/saiyadaz/expense-ansible2.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
